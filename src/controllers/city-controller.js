a373a4896a5fe4bdf288df33d53ae139b2b19f0e
Varad Gupta <varadgupta21@gmail.com>
2023-11-07 20:25:30 +0530

    Added City Controller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,82 @@
+const {CityService} = require('../services/index')
+const cityService = new CityService()
+const create = async (req,res)=>{
+  try{
+    const city  = await cityService.createCity(req.body)
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message : 'Successfully generated',
+      err: {}
+    })
+  }catch(error){
+    console.log(error)
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Not able to create a city',
+      err: {error}
+    })
+  }
+}
+const destroy = async (req,res)=>{
+  try{
+    const response  = await cityService.deleteCity(req.params.id)
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message : 'Successfully deleted a city',
+      err: {}
+    })
+  }catch(error){
+    console.log(error)
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Not able to delete the city',
+      err: {error}
+    })
+  }
+}
+const update = async (req,res)=>{
+  try{
+    const response  = await cityService.updateCity(req.params.id, req.body)
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message : 'Successfully deleted a city',
+      err: {}
+    })
+  }catch(error){
+    console.log(error)
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Not able to delete the city',
+      err: {error}
+    })
+  }
+}
+const get = async(req,res)=>{
+  try{
+    const response  = await cityService.getCity(req.params.id)
+    return res.status(201).json({
+      data: response,
+      success: true,
+      message : 'Successfully generated',
+      err: {}
+    })
+  }catch(error){
+    console.log(error)
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: 'Not able to get a city',
+      err: {error}
+    })
+  }
+}
+
+module.exports = {
+  create,update,destroy,get
+}
